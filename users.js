bed6a89c7e964415b47cb81a9bb3267865d94ba1
elmuss2 <elmus2@pdx.edu>
2023-11-14 20:13:00 -0800

    adding display and modifying the delete functionality with css formatting

--- users.js
+++ users.js
@@ -10,6 +10,7 @@ web.use(cors());
 //parses the input into json
 web.use(bodyParser.json());
 web.use(bodyParser.urlencoded({ extended: true }));
+web.use(express.json());
 
 web.post('/adduser',async (req, res) => {
     const { Fname, Lname } = req.body;
@@ -47,8 +48,14 @@ web.delete('/deleteuser', async (req, res)=>{
     });
     try{
         await client.connect();
-        await client.query('DELETE FROM users WHERE fname = $1 AND lname = $2', [Fname, Lname]);
-        res.send("User Deleted")
+        console.log(' data:', { Fname, Lname });
+        const result = await client.query('DELETE FROM users WHERE TRIM(fname) = TRIM($1) AND TRIM(lname) = TRIM($2) RETURNING *',[Fname, Lname]);
+
+        if (result.rowCount > 0) {
+            res.send("User Deleted");
+        } else {
+            res.status(404).send("User not found");
+        }
     }
     catch(error)
     {
@@ -59,7 +66,27 @@ web.delete('/deleteuser', async (req, res)=>{
         await client.end();
     }
 })
-
+web.get('/displayusers', async (req,res)=>{
+    const client = new Client({
+        user: "postgres",
+        password: "gKstv5-h3b",
+        host: hostname,
+        port: 5432,
+        database: "test"
+    });
+    try{
+        await client.connect();
+        const result = await client.query('SELECT * FROM users');
+        res.send(result.rows);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send('error while deleting data' + error.message)
+    }
+    finally{
+        await client.end();
+    }
+})
 web.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
